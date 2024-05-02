203e187194949d37b81d443bcccbec1d569fc425
LiamGunning07 <liamgunning07@gmail.com>
2024-05-02 16:23:58 -0600

    Second exercise completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the first store with id = 1 and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store from the database and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the first store (@store1) instance in the database
+@store1.update(name: 'Burnaby Bestsellers')
+
+# Output the updated name of @store1 to verify the update
+puts "Updated name for @store1: #{@store1.name}"
